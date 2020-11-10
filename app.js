ba4599299a9ed940ef2c0d42e121dcc137395db3
José C. Pernías <jcpernias@gmail.com>
2020-11-10 10:44:27 +0100

    Access html elements

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let gridDiv = document.querySelector('.grid');
-    for (let i = 0; i < 200; i++) {
+    // Grid dimensions
+    const width = 10;
+    const height = 20;
+    const numSquares = width * height;
+
+    // Build grid
+    const gridDiv = document.querySelector('.grid');
+    for (let i = 0; i < numSquares; i++) {
         const element = document.createElement('div');
         gridDiv.appendChild(element)
     }
     document.body.appendChild(gridDiv);
+
+    // Access to html elements
+    let squares = Array.from(gridDiv.querySelectorAll('div'));
+    let score = document.querySelector('#score');
+    let startButton = document.querySelector('#start-button');
 })
